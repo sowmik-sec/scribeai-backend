b1d6453813b0a19d7a030cc84df2e8e3e714328b
sowmik-sec <sowmik.sec@gmail.com>
2025-08-14 11:42:32 +0600

    feat: configure server port using config file

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
 import { Server } from "http";
 import app from "./app";
+import config from "./config";
 
-const port = 5000;
-
+const port = config.port;
 async function main() {
   const server: Server = app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
