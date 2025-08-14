146a667e6e106a3a2597af492e9594f08f0585eb
sowmik-sec <sowmik.sec@gmail.com>
2025-08-14 21:46:08 +0600

    feat: add authentication middleware and extend Express Request interface to include user information

---
+++ src/app/middlewares/auth.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response } from "express";
+import ApiError from "../../errors/ApiError";
+import { StatusCodes } from "http-status-codes";
+import { jwtHelpers } from "../../helpers/jwtHelpers";
+import config from "../../config";
+import { Secret } from "jsonwebtoken";
+
+const auth = () => async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const token = req.headers.authorization;
+    if (!token) {
+      throw new ApiError(StatusCodes.UNAUTHORIZED, "You are not authorized");
+    }
+    let verifiedUser = null;
+    verifiedUser = jwtHelpers.verifyToken(token, config.jwt.secret as Secret);
+    req.user = verifiedUser;
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
